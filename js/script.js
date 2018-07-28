a4be6f5a6aac1956d580f7d439cad3fd5b60cba0
Emily Mattison <egmatti@gmail.com>
2018-07-28 14:07:14 -0400

    Got header JavaScript working without hover effects

--- js/script.js
+++ js/script.js
@@ -6,6 +6,7 @@
 // HEADER
 
 $(document).ready(function(){
+
     $(function () {
         $("header").addClass("header--default");
         $(".main-nav--left li").addClass("nav-items--default");
@@ -13,66 +14,31 @@ $(document).ready(function(){
 
         $(window).on("scroll", function () {
 
-            if ($(window).scrollTop() < 50 && $('.main-nav li').mouseout()) {
+            if ($(window).scrollTop() < 50) {
                 $("header").addClass("header--default");
                 $(".main-nav--left li").addClass("nav-items--default");
                 $(".main-nav--right li").addClass("nav-items--default");
                 $("header").removeClass("header--scrolled");
                 $(".main-nav--left li").removeClass("nav-items--scrolled");
                 $(".main-nav--right li").removeClass("nav-items--scrolled");
-                $(".main-nav--left li").css({ color: "#FFFFFF" });
-                $(".main-nav--right li").css({ color: "#FFFFFF" });
-                console.log("default");
-            } else if ($(window).scrollTop() < 50 && $(".main-nav--left li").mouseover()) {
-                $("header").addClass("header--default");
-                $(".main-nav--left li").addClass("nav-items--default");
-                $(".main-nav--right li").addClass("nav-items--default");
-                $("header").removeClass("header--scrolled");
-                $(".main-nav--left li").removeClass("nav-items--scrolled");
-                $(".main-nav--right li").removeClass("nav-items--scrolled");
-                $(".main-nav--left li").css({ color: "#BFBFBF" });
-                $(this).css({ color: "#FFFFFF" });
-                console.log("default--left");
-            } else if ($(window).scrollTop() < 50 && $(".main-nav--right li").mouseover()) {
-                $("header").addClass("header--default");
-                $(".main-nav--left li").addClass("nav-items--default");
-                $(".main-nav--right li").addClass("nav-items--default");
-                $("header").removeClass("header--scrolled");
-                $(".main-nav--left li").removeClass("nav-items--scrolled");
-                $(".main-nav--right li").removeClass("nav-items--scrolled");
-                $(".main-nav--right li").css({ color: "#BFBFBF" });
-                $(this).css({ color: "#FFFFFF" });
-                console.log("default--right");
-            } else if ($(window).scrollTop() > 50 && $('.main-nav li').mouseout()) {
+                $(".logo").attr("src", "images/logo-white.png");
+                $(".search").attr("src", "images/search-white.svg");
+                $(".bag").attr("src", "images/bag-white.svg");
+                $(".search").css(("transform", "translateY(3px)"));
+                $(".bag").css(("transform", "translateY(7.5px)"));
+                
+            } else if ($(window).scrollTop() > 50) {
                 $("header").addClass("header--scrolled");
                 $(".main-nav--left li").addClass("nav-items--scrolled");
                 $(".main-nav--right li").addClass("nav-items--scrolled");
                 $("header").removeClass("header--default");
                 $(".main-nav--left li").removeClass("nav-items--default");
                 $(".main-nav--right li").removeClass("nav-items--default");
-                $(".main-nav--left li").css({ color: "#222222" });
-                $(".main-nav--right li").css({ color: "#222222" });
-                console.log("scrolled");
-            } else if ($(window).scrollTop() > 50 && $(".main-nav--left li").mouseover()) {
-                $("header").addClass("header--scrolled");
-                $(".main-nav--left li").addClass("nav-items--scrolled");
-                $(".main-nav--right li").addClass("nav-items--scrolled");
-                $("header").removeClass("header--default");
-                $(".main-nav--left li").removeClass("nav-items--default");
-                $(".main-nav--right li").removeClass("nav-items--default");
-                $(".main-nav--left li").css({ color: "#BFBFBF" });
-                $(this).css({ color: "#222222" });
-                console.log("scrolled--left");
-            } else if ($(window).scrollTop() > 50 && $(".main-nav--right li").mouseover()) {
-                $("header").addClass("header--scrolled");
-                $(".main-nav--left li").addClass("nav-items--scrolled");
-                $(".main-nav--right li").addClass("nav-items--scrolled");
-                $("header").removeClass("header--default");
-                $(".main-nav--left li").removeClass("nav-items--default");
-                $(".main-nav--right li").removeClass("nav-items--default");
-                $(".main-nav--right li").css({color: "#BFBFBF"});
-                $(this).css({ color: "#222222" });
-                console.log("scrolled--right");
+                $(".logo").attr("src", "images/logo-black.png");
+                $(".search").attr("src", "images/search-black.svg");
+                $(".bag").attr("src", "images/bag-black.svg");
+                $(".search").css("transform", "translateY(5px)");
+                $(".bag").css(("transform", "translateY(-7.5px)"));
             }
         });
     });
