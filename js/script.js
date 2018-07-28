fa5b9a977261fd55800eb155dc7a555da87a439f
Emily Mattison <egmatti@gmail.com>
2018-07-28 11:36:50 -0400

    Trying to fix JavaScript on header

--- js/script.js
+++ js/script.js
@@ -10,65 +10,69 @@ $(document).ready(function(){
         $("header").addClass("header--default");
         $(".main-nav--left li").addClass("nav-items--default");
         $(".main-nav--right li").addClass("nav-items--default");
+
         $(window).on("scroll", function () {
-            if ($(window).scrollTop() > 50) {
-                $("header").addClass("header--scrolled");
-                $(".main-nav--left li").addClass("nav-items--scrolled");
-                $(".main-nav--right li").addClass("nav-items--scrolled");
-                $("header").removeClass("header--default");
-                $(".main-nav--left li").removeClass("nav-items--default");
-                $(".main-nav--right li").removeClass("nav-items--default");
 
-                // $(".main-nav--left li").mouseover(
-                //     function () {
-                //         $(".main-nav--left li").css({ color: "#BFBFBF" });
-                //         $(this).css({ color: "#222222" });
-                //     }
-                // );
-                // $('.main-nav--left li').mouseout(
-                //     function () {
-                //         $(".main-nav--left li").css({ "color": "#222222" });
-                //     });
-
-                // $('.main-nav--right li').mouseover(
-                //     function () {
-                //         $(".main-nav--right li").css({ "color": "#BFBFBF" });
-                //         $(this).css({ "color": "#222222" });
-                //     });
-                // $('.main-nav--right li').mouseout(
-                //     function () {
-                //         $(".main-nav--right li").css({ "color": "#222222" });
-                //     });
-
-            } else {
+            if ($(window).scrollTop() < 50 && $('.main-nav li').mouseout()) {
                 $("header").addClass("header--default");
                 $(".main-nav--left li").addClass("nav-items--default");
                 $(".main-nav--right li").addClass("nav-items--default");
                 $("header").removeClass("header--scrolled");
                 $(".main-nav--left li").removeClass("nav-items--scrolled");
                 $(".main-nav--right li").removeClass("nav-items--scrolled");
-
-                // $(".main-nav--left li").mouseover(
-                //     function () {
-                //         $(".main-nav--left li").css({ color: "#BFBFBF" });
-                //         $(this).css({ color: "#FFFFFF" });
-                //     }
-                // );
-                // $('.main-nav--left li').mouseout(
-                //     function () {
-                //         $(".main-nav--left li").css({ "color": "#FFFFFF" });
-                //     });
-
-                // $('.main-nav--right li').mouseover(
-                //     function () {
-                //         $(".main-nav--right li").css({ "color": "#BFBFBF" });
-                //         $(this).css({ "color": "#FFFFFF" });
-                //     });
-                // $('.main-nav--right li').mouseout(
-                //     function () {
-                //         $(".main-nav--right li").css({ "color": "#FFFFFF" });
-                //     });
-
+                $(".main-nav--left li").css({ color: "#FFFFFF" });
+                $(".main-nav--right li").css({ color: "#FFFFFF" });
+                console.log("default");
+            } else if ($(window).scrollTop() < 50 && $(".main-nav--left li").mouseover()) {
+                $("header").addClass("header--default");
+                $(".main-nav--left li").addClass("nav-items--default");
+                $(".main-nav--right li").addClass("nav-items--default");
+                $("header").removeClass("header--scrolled");
+                $(".main-nav--left li").removeClass("nav-items--scrolled");
+                $(".main-nav--right li").removeClass("nav-items--scrolled");
+                $(".main-nav--left li").css({ color: "#BFBFBF" });
+                $(this).css({ color: "#FFFFFF" });
+                console.log("default--left");
+            } else if ($(window).scrollTop() < 50 && $(".main-nav--right li").mouseover()) {
+                $("header").addClass("header--default");
+                $(".main-nav--left li").addClass("nav-items--default");
+                $(".main-nav--right li").addClass("nav-items--default");
+                $("header").removeClass("header--scrolled");
+                $(".main-nav--left li").removeClass("nav-items--scrolled");
+                $(".main-nav--right li").removeClass("nav-items--scrolled");
+                $(".main-nav--right li").css({ color: "#BFBFBF" });
+                $(this).css({ color: "#FFFFFF" });
+                console.log("default--right");
+            } else if ($(window).scrollTop() > 50 && $('.main-nav li').mouseout()) {
+                $("header").addClass("header--scrolled");
+                $(".main-nav--left li").addClass("nav-items--scrolled");
+                $(".main-nav--right li").addClass("nav-items--scrolled");
+                $("header").removeClass("header--default");
+                $(".main-nav--left li").removeClass("nav-items--default");
+                $(".main-nav--right li").removeClass("nav-items--default");
+                $(".main-nav--left li").css({ color: "#222222" });
+                $(".main-nav--right li").css({ color: "#222222" });
+                console.log("scrolled");
+            } else if ($(window).scrollTop() > 50 && $(".main-nav--left li").mouseover()) {
+                $("header").addClass("header--scrolled");
+                $(".main-nav--left li").addClass("nav-items--scrolled");
+                $(".main-nav--right li").addClass("nav-items--scrolled");
+                $("header").removeClass("header--default");
+                $(".main-nav--left li").removeClass("nav-items--default");
+                $(".main-nav--right li").removeClass("nav-items--default");
+                $(".main-nav--left li").css({ color: "#BFBFBF" });
+                $(this).css({ color: "#222222" });
+                console.log("scrolled--left");
+            } else if ($(window).scrollTop() > 50 && $(".main-nav--right li").mouseover()) {
+                $("header").addClass("header--scrolled");
+                $(".main-nav--left li").addClass("nav-items--scrolled");
+                $(".main-nav--right li").addClass("nav-items--scrolled");
+                $("header").removeClass("header--default");
+                $(".main-nav--left li").removeClass("nav-items--default");
+                $(".main-nav--right li").removeClass("nav-items--default");
+                $(".main-nav--right li").css({color: "#BFBFBF"});
+                $(this).css({ color: "#222222" });
+                console.log("scrolled--right");
             }
         });
     });
