ce698dae7bffc9a5559b37db607bd74cef08b609
Emily Mattison <egmatti@gmail.com>
2018-07-30 22:40:25 -0400

    Started adding About and Instagram sections to home page

--- js/home.js
+++ js/home.js
@@ -19,5 +19,28 @@ $(document).ready(function(){
         $("#popular-product-2").attr("src", "images/home/popular-products/product-2/main-image.png");
     });
 
-    
+    $("#popular-product-3").mouseover(function () {
+        $("#popular-product-3").attr("src", "images/home/popular-products/product-3/alternate-image.png");
+    });
+    $("#popular-product-3").mouseout(function () {
+        $("#popular-product-3").attr("src", "images/home/popular-products/product-3/main-image.png");
+    });
+
+    $("#popular-product-4").mouseover(function () {
+        $("#popular-product-4").attr("src", "images/home/popular-products/product-4/alternate-image.png");
+    });
+    $("#popular-product-4").mouseout(function () {
+        $("#popular-product-4").attr("src", "images/home/popular-products/product-4/main-image.png");
+    }); 
+});
+
+
+// INSTAGRAM
+
+$(".instagram-slider-for--desktop").slick({
+    arrows: true,
+    infinite: true,
+    slidesToScroll: 1,
+    slidesToShow: 3,
+    vertical: false
 });
