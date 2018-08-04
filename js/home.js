1b7b18120820251c91c8f6c8b5590b54cab0c509
Emily Mattison <egmatti@gmail.com>
2018-08-04 15:04:00 -0400

    Fixed scroll functionality on header so that it looks different on other pages

--- js/home.js
+++ js/home.js
@@ -2,6 +2,57 @@
 // HOME PAGE
 // ==========================================
 
+// HEADER
+
+$(document).ready(function () {
+
+    if ($("body").is("#home-page")) {
+        
+        $(function () {
+            $("header").addClass("header--default--home");
+            $(".logo").addClass("logo--default--home");
+            $(".main-nav--left li").addClass("nav-items--default--home");
+            $(".main-nav--right li").addClass("nav-items--default--home");
+            $(".logo").attr("src", "images/main-nav/logo-white.png");
+            $(".search").attr("src", "images/main-nav/search-white.svg");
+            $(".bag").attr("src", "images/main-nav/bag-white.svg");
+
+            $(window).on("scroll", function() {
+                if ($(window).scrollTop() < 50) {
+                    $("header").addClass("header--default--home");
+                    $(".logo").addClass("logo--default--home");
+                    $(".main-nav--left li").addClass("nav-items--default--home");
+                    $(".main-nav--right li").addClass("nav-items--default--home");
+                    $("header").removeClass("header--scrolled");
+                    $(".logo").removeClass("logo--scrolled");
+                    $(".main-nav--left li").removeClass("nav-items--scrolled");
+                    $(".main-nav--right li").removeClass("nav-items--scrolled");
+                    $(".logo").attr("src", "images/main-nav/logo-white.png");
+                    $(".search").attr("src", "images/main-nav/search-white.svg");
+                    $(".bag").attr("src", "images/main-nav/bag-white.svg");
+                    $(".search").css(("transform", "translateY(3px)"));
+                    $(".bag").css(("transform", "translateY(7.5px)"));
+                } else if ($(window).scrollTop() > 50) {
+                    $("header").addClass("header--scrolled");
+                    $(".logo").addClass("logo--scrolled");
+                    $(".main-nav--left li").addClass("nav-items--scrolled");
+                    $(".main-nav--right li").addClass("nav-items--scrolled");
+                    $("header").removeClass("header--default--home");
+                    $(".logo").removeClass("logo--default--home");
+                    $(".main-nav--left li").removeClass("nav-items--default--home");
+                    $(".main-nav--right li").removeClass("nav-items--default--home");
+                    $(".logo").attr("src", "images/main-nav/wordmark-black.svg");
+                    $(".search").attr("src", "images/main-nav/search-black.svg");
+                    $(".bag").attr("src", "images/main-nav/bag-black.svg");
+                    $(".search").css("transform", "translateY(5px)");
+                    $(".bag").css(("transform", "translateY(-7.5px)"));
+                }
+            });
+        });
+    }
+});
+
+
 // POPULAR PRODUCTS
 
 $(document).ready(function(){
--- js/script.js
+++ js/script.js
@@ -7,46 +7,48 @@
 
 $(document).ready(function(){
 
-    $(function () {
-        $("header").addClass("header--default");
-        $(".logo").addClass("logo--default");
-        $(".main-nav--left li").addClass("nav-items--default");
-        $(".main-nav--right li").addClass("nav-items--default");
+    if ($("body").not("#home-page")) {
 
-        $(window).on("scroll", function () {
+        $(function () {
+            $("header").addClass("header--default");
+            $(".logo").addClass("logo--default");
+            $(".main-nav--left li").addClass("nav-items--default");
+            $(".main-nav--right li").addClass("nav-items--default");
+            $(".logo").attr("src", "images/main-nav/logo-black.png");
+            $(".search").attr("src", "images/main-nav/search-black.svg");
+            $(".bag").attr("src", "images/main-nav/bag-black.svg");
 
-            if ($(window).scrollTop() < 50) {
-                $("header").addClass("header--default");
-                $(".logo").addClass("logo--default");
-                $(".main-nav--left li").addClass("nav-items--default");
-                $(".main-nav--right li").addClass("nav-items--default");
-                $("header").removeClass("header--scrolled");
-                $(".logo").removeClass("logo--scrolled");
-                $(".main-nav--left li").removeClass("nav-items--scrolled");
-                $(".main-nav--right li").removeClass("nav-items--scrolled");
-                $(".logo").attr("src", "images/main-nav/logo-white.png");
-                $(".search").attr("src", "images/main-nav/search-white.svg");
-                $(".bag").attr("src", "images/main-nav/bag-white.svg");
-                $(".search").css(("transform", "translateY(3px)"));
-                $(".bag").css(("transform", "translateY(7.5px)"));
-                
-            } else if ($(window).scrollTop() > 50) {
-                $("header").addClass("header--scrolled");
-                $(".logo").addClass("logo--scrolled");
-                $(".main-nav--left li").addClass("nav-items--scrolled");
-                $(".main-nav--right li").addClass("nav-items--scrolled");
-                $("header").removeClass("header--default");
-                $(".logo").removeClass("logo--default");
-                $(".main-nav--left li").removeClass("nav-items--default");
-                $(".main-nav--right li").removeClass("nav-items--default");
-                $(".logo").attr("src", "images/main-nav/wordmark-black.svg");
-                $(".search").attr("src", "images/main-nav/search-black.svg");
-                $(".bag").attr("src", "images/main-nav/bag-black.svg");
-                $(".search").css("transform", "translateY(5px)");
-                $(".bag").css(("transform", "translateY(-7.5px)"));
-            }
+            $(window).on("scroll", function () {
+
+                if ($(window).scrollTop() < 50) {
+                    $("header").addClass("header--default");
+                    $(".logo").addClass("logo--default");
+                    $(".main-nav--left li").addClass("nav-items--default");
+                    $(".main-nav--right li").addClass("nav-items--default");
+                    $("header").removeClass("header--scrolled");
+                    $(".logo").removeClass("logo--scrolled");
+                    $(".main-nav--left li").removeClass("nav-items--scrolled");
+                    $(".main-nav--right li").removeClass("nav-items--scrolled");
+                    $(".logo").attr("src", "images/main-nav/logo-black.png");
+                    $(".search").css(("transform", "translateY(3px)"));
+                    $(".bag").css(("transform", "translateY(7.5px)"));
+
+                } else if ($(window).scrollTop() > 50) {
+                    $("header").addClass("header--scrolled");
+                    $(".logo").addClass("logo--scrolled");
+                    $(".main-nav--left li").addClass("nav-items--scrolled");
+                    $(".main-nav--right li").addClass("nav-items--scrolled");
+                    $("header").removeClass("header--default");
+                    $(".logo").removeClass("logo--default");
+                    $(".main-nav--left li").removeClass("nav-items--default");
+                    $(".main-nav--right li").removeClass("nav-items--default");
+                    $(".logo").attr("src", "images/main-nav/wordmark-black.svg");
+                    $(".search").css("transform", "translateY(5px)");
+                    $(".bag").css(("transform", "translateY(-7.5px)"));
+                }
+            });
         });
-    });
+    }
 });
 
 
