f7b3588266db2efa3ffd71c98b0d1f28ac3b0e94
Emily Mattison <egmatti@gmail.com>
2018-09-02 21:09:33 -0400

    Finished creating Payment page of checkout flow

--- js/checkout.js
+++ js/checkout.js
@@ -1,3 +1,15 @@
 // ==========================================
 // CHECKOUT PAGES
 // ==========================================
+
+
+$(document).ready(function(){
+    $(".billing-address-form .radio-button").click(function () {
+        if ($("#new-address").is(":checked")) {
+            $(".billing-address-fieldset").css("display", "block");
+        } else {
+            $(".billing-address-fieldset").css("display", "none");
+        }
+    }); 
+});
+
